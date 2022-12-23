258ea2527a783627389fa1ff24a0e11121cc87b5
mesutalkassum1 <masood.alkassom@ogr.sakarya.edu.tr>
2022-12-23 15:54:35 +0300

    .

---
+++ Barcelona-Store/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Barcelona_Store.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barcelona_Store.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
