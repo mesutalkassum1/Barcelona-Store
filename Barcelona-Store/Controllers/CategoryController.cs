1838f1625644c14260d95205b559c4afc70cce9e
mesutalkassum1 <masood.alkassom@ogr.sakarya.edu.tr>
2022-12-24 17:40:46 +0300

    .

--- Barcelona-Store/Controllers/CategoryController.cs
+++ Barcelona-Store/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Barcelona_Store.Data;
+using Barcelona_Store.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Barcelona_Store.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _context.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
