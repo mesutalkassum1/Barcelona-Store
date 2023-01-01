165fd56b8fe78b373d32d46ec6d3b5d60f2a7966
mesutalkassum1 <masood.alkassom@ogr.sakarya.edu.tr>
2023-01-01 17:41:23 +0300

    .

--- BarcelonaStore.Utility/EmailSender.cs
+++ BarcelonaStore.Utility/EmailSender.cs
@@ -1,5 +1,4 @@
-﻿using MailKit.Net.Smtp;
-using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using MimeKit;
 using System;
 using System.Collections.Generic;
@@ -13,22 +12,22 @@ namespace BarcelonaStore.Utility
 	{
 		public Task SendEmailAsync(string email, string subject, string htmlMessage)
 		{
-			var emailToSend = new MimeMessage();
-			emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
-			emailToSend.To.Add(MailboxAddress.Parse(email));
-			emailToSend.Subject = subject;
-			emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+			//var emailToSend = new MimeMessage();
+			//emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+			//emailToSend.To.Add(MailboxAddress.Parse(email));
+			//emailToSend.Subject = subject;
+			//emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
 
-			//send email
-			using (var emailClient = new SmtpClient())
-			{
-				emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-				emailClient.Authenticate("masudkasom1@gmail.com", "tdk555262");
-				emailClient.Send(emailToSend);
-				emailClient.Disconnect(true);
-			}
-
+			////send email
+			//using (var emailClient = new SmtpClient())
+			//{
+			//	emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+			//	emailClient.Authenticate("masudkasom1@gmail.com", "tdk555262");
+			//	emailClient.Send(emailToSend);
+			//	emailClient.Disconnect(true);
+			//}
 			return Task.CompletedTask;
+
 		}
 	}
 }
